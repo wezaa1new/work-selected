801b53ba45c8aa602d2ab0be264912506bb047cd
NongNew <110647116+wezaa1new@users.noreply.github.com>
2022-08-19 12:00:18 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const divStyle = {
+    color:'red',
+    backgroundColor:'powderblue',
+    fontSize:'larger',
+    padding:'10px',
+    margin:'50px'
+  }
+  return <div style={divStyle}>Hello React</div> 
+}
+
+export default App;
